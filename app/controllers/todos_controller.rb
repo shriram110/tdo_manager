6b1934a0fbbeac4b1e4270ecf32f654ef2c543ba
shriram110 <shrirambharadwajsb@gmail.com>
2020-04-18 20:11:50 +0530

    level7 resubmit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -14,7 +14,7 @@ class TodosController < ApplicationController
   def create
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: true)
     redirect_to todos_path
   end
 

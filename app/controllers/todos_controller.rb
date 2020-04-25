e374c46826668c4a120cb5e665320652727a5037
shriram110 <shrirambharadwajsb@gmail.com>
2020-04-25 11:08:04 +0530

    formatted erb files

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,4 @@
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def index
     render "index"
     #render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
@@ -16,7 +14,7 @@ class TodosController < ApplicationController
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text,
                             due_date: due_date,
-                            completed: true)
+                            completed: false)
     redirect_to todos_path
   end
 
@@ -26,6 +24,13 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "updated todo completed status of id #{id} to #{completed} "
+    redirect_to todos_path
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
   end
 end

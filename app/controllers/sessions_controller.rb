7bfb081c414d366846bed4dcc4dd09afdeead300
shriram110 <shrirambharadwajsb@gmail.com>
2020-05-06 13:53:24 +0530

    login redirected to todos page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
@@ -6,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:current_user_id] = user.id
-      render plain: "You have entered the Correct Password"
+      redirect_to "/"
     else
       render plain: "Incorrect Password"
     end

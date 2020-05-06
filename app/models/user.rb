46e1abf994cf77f96f997a767bdfbd93fe13bd28
shriram110 <shrirambharadwajsb@gmail.com>
2020-05-06 23:05:06 +0530

    invoked flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :email, presence: true
-  validates :todo_text, length: { minimum: 1 }
+  validates :first_name, length: { minimum: 1 }
   has_secure_password
   has_many :todos
 end
